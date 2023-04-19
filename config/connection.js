d29838cb671864ff727d64f26141b74e7f122639
hirasoomro <hiraasoomro@gmail.com>
2023-04-19 09:41:05 -0700

    updated minor fixes to get server running

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
+//console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: "localhost",
+    host: "127.0.0.1",
     dialect: 'mysql',
-    port: "3001",
+    port: 3306,
   }
 );
 
