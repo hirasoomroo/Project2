5597c044421ee24d669606869852d1472aa3ff93
hirasoomro <hiraasoomro@gmail.com>
2023-04-23 14:26:25 -0700

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-//console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -14,4 +12,7 @@ const sequelize = new Sequelize(
   }
 );
 
+
 module.exports = sequelize;
+
+
