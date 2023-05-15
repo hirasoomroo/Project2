ed6505098a01c6d8260a2b2a54be90ca98cb831c
hirasoomro <hiraasoomro@gmail.com>
2023-05-15 11:15:02 -0700

    updaitng json package

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,16 @@
-const { Sequelize } = require("sequelize");
-requie('dotenv').config();
+const Sequelize = require('sequelize');
+require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new 
-  Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWWORD,
+    process.env.DB_PASSWORD,
     {
-      host: '127.0.0.1',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306,
     },
